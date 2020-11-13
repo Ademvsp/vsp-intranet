9bb5f787209cd0888467f257637f010151667bc0
andrewvo89 <48583281+andrewvo89@users.noreply.github.com>
2020-11-14 06:08:32 +1100

    Update eslint versions and configuration files

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const dotenv = require('dotenv');
 const admin = require('firebase-admin');
+//Initialize upon starting server
+dotenv.config();
+admin.initializeApp();
+admin.firestore().settings({
+  ignoreUndefinedProperties: true
+});
 
 const {
   getAuthPhoneNumber,
@@ -10,60 +16,53 @@ const {
   revokeRefreshTokens
 } = require('./functions/auth.js');
 const { getServerTimeInMilliseconds } = require('./utils/date');
-const {
-  leaveRequestCreateListener,
-  leaveRequestUpdateListener
-} = require('./firestore-listeners/leave-requests');
-const {
-  notificationCreateListener
-} = require('./firestore-listeners/notifications');
-const { customerCreateListener } = require('./firestore-listeners/customers');
-const {
-  productRequestCreateListener,
-  productRequestUpdateListener
-} = require('./firestore-listeners/product-requests.js');
-const {
-  eventCreateListener,
-  eventUpdateListener,
-  eventDeleteListener
-} = require('./firestore-listeners/events.js');
-const {
-  projectCreateListener,
-  projectUpdateListener
-} = require('./firestore-listeners/projects.js');
-const {
-  postCreateListener,
-  postUpdateListener
-} = require('./firestore-listeners/posts.js');
-const { vendorCreateListener } = require('./firestore-listeners/vendors.js');
-const {
-  expenseClaimCreateListener,
-  expenseClaimUpdateListener
-} = require('./firestore-listeners/expense-claims.js');
-const {
-  promotionCreateListener,
-  promotionUpdateListener,
-  promotionDeleteListener
-} = require('./firestore-listeners/promotions.js');
-const {
-  jobDocumentCreateListener,
-  jobDocumentUpdateListener,
-  jobDocumentDeleteListener
-} = require('./firestore-listeners/job-documents.js');
-const {
-  firmwareCreateListener,
-  firmwareUpdateListener,
-  firmwareDeleteListener
-} = require('./firestore-listeners/firmwares.js');
-const { userCreateListener } = require('./firestore-listeners/users.js');
-const { dailyTasks } = require('./functions/schedule.js');
-
-//Initialize upon starting server
-dotenv.config();
-admin.initializeApp();
-admin.firestore().settings({
-  ignoreUndefinedProperties: true
-});
+// const {
+//   leaveRequestCreateListener,
+//   leaveRequestUpdateListener
+// } = require('./firestore-listeners/leave-requests');
+// const {
+//   notificationCreateListener
+// } = require('./firestore-listeners/notifications');
+// const { customerCreateListener } = require('./firestore-listeners/customers');
+// const {
+//   productRequestCreateListener,
+//   productRequestUpdateListener
+// } = require('./firestore-listeners/product-requests.js');
+// const {
+//   eventCreateListener,
+//   eventUpdateListener,
+//   eventDeleteListener
+// } = require('./firestore-listeners/events.js');
+// const {
+//   projectCreateListener,
+//   projectUpdateListener
+// } = require('./firestore-listeners/projects.js');
+// const {
+//   postCreateListener,
+//   postUpdateListener
+// } = require('./firestore-listeners/posts.js');
+// const { vendorCreateListener } = require('./firestore-listeners/vendors.js');
+// const {
+//   expenseClaimCreateListener,
+//   expenseClaimUpdateListener
+// } = require('./firestore-listeners/expense-claims.js');
+// const {
+//   promotionCreateListener,
+//   promotionUpdateListener,
+//   promotionDeleteListener
+// } = require('./firestore-listeners/promotions.js');
+// const {
+//   jobDocumentCreateListener,
+//   jobDocumentUpdateListener,
+//   jobDocumentDeleteListener
+// } = require('./firestore-listeners/job-documents.js');
+// const {
+//   firmwareCreateListener,
+//   firmwareUpdateListener,
+//   firmwareDeleteListener
+// } = require('./firestore-listeners/firmwares.js');
+// const { userCreateListener } = require('./firestore-listeners/users.js');
+// const { dailyTasks } = require('./functions/schedule.js');
 
 //Authentication functions
 module.exports.authFunctions = {
@@ -79,60 +78,60 @@ module.exports.authFunctions = {
 module.exports.getServerTimeInMilliseconds = getServerTimeInMilliseconds;
 
 //Scheduled functions
-module.exports.scheduledFunctions = {
-  dailyTasks: dailyTasks
-};
+// module.exports.scheduledFunctions = {
+//   dailyTasks: dailyTasks
+// };
 
 //Firestore listeners
-module.exports.usersFirestoreListener = {
-  userCreateListener: userCreateListener
-};
-module.exports.customersFirestoreListener = {
-  customerCreateListener: customerCreateListener
-};
-module.exports.vendorsFirestoreListener = {
-  vendorCreateListener: vendorCreateListener
-};
-module.exports.notificationsFirestoreListeners = {
-  notificationCreateListener: notificationCreateListener
-};
-module.exports.postsFirestoreListener = {
-  postCreateListener: postCreateListener,
-  postUpdateListener: postUpdateListener
-};
-module.exports.eventsFirestoreListeners = {
-  eventCreateListener: eventCreateListener,
-  eventUpdateListener: eventUpdateListener,
-  eventDeleteListener: eventDeleteListener
-};
-module.exports.projectsFirestoreListeners = {
-  projectCreateListener: projectCreateListener,
-  projectUpdateListener: projectUpdateListener
-};
-module.exports.leaveRequestsFirestoreListeners = {
-  leaveRequestCreateListener: leaveRequestCreateListener,
-  leaveRequestUpdateListener: leaveRequestUpdateListener
-};
-module.exports.productRequestsFirestoreListeners = {
-  productRequestCreateListener: productRequestCreateListener,
-  productRequestUpdateListener: productRequestUpdateListener
-};
-module.exports.expenseClaimsFirestoreListeners = {
-  expenseClaimCreateListener: expenseClaimCreateListener,
-  expenseClaimUpdateListener: expenseClaimUpdateListener
-};
-module.exports.promotionsFirestoreListener = {
-  promotionCreateListener: promotionCreateListener,
-  promotionUpdateListener: promotionUpdateListener,
-  promotionDeleteListener: promotionDeleteListener
-};
-module.exports.jobDocumentsFirestoreListener = {
-  jobDocumentCreateListener: jobDocumentCreateListener,
-  jobDocumentUpdateListener: jobDocumentUpdateListener,
-  jobDocumentDeleteListener: jobDocumentDeleteListener
-};
-module.exports.firmwaresFirestorListener = {
-  firmwareCreateListener: firmwareCreateListener,
-  firmwareUpdateListener: firmwareUpdateListener,
-  firmwareDeleteListener: firmwareDeleteListener
-};
+// module.exports.usersFirestoreListener = {
+//   userCreateListener: userCreateListener
+// };
+// module.exports.customersFirestoreListener = {
+//   customerCreateListener: customerCreateListener
+// };
+// module.exports.vendorsFirestoreListener = {
+//   vendorCreateListener: vendorCreateListener
+// };
+// module.exports.notificationsFirestoreListeners = {
+//   notificationCreateListener: notificationCreateListener
+// };
+// module.exports.postsFirestoreListener = {
+//   postCreateListener: postCreateListener,
+//   postUpdateListener: postUpdateListener
+// };
+// module.exports.eventsFirestoreListeners = {
+//   eventCreateListener: eventCreateListener,
+//   eventUpdateListener: eventUpdateListener,
+//   eventDeleteListener: eventDeleteListener
+// };
+// module.exports.projectsFirestoreListeners = {
+//   projectCreateListener: projectCreateListener,
+//   projectUpdateListener: projectUpdateListener
+// };
+// module.exports.leaveRequestsFirestoreListeners = {
+//   leaveRequestCreateListener: leaveRequestCreateListener,
+//   leaveRequestUpdateListener: leaveRequestUpdateListener
+// };
+// module.exports.productRequestsFirestoreListeners = {
+//   productRequestCreateListener: productRequestCreateListener,
+//   productRequestUpdateListener: productRequestUpdateListener
+// };
+// module.exports.expenseClaimsFirestoreListeners = {
+//   expenseClaimCreateListener: expenseClaimCreateListener,
+//   expenseClaimUpdateListener: expenseClaimUpdateListener
+// };
+// module.exports.promotionsFirestoreListener = {
+//   promotionCreateListener: promotionCreateListener,
+//   promotionUpdateListener: promotionUpdateListener,
+//   promotionDeleteListener: promotionDeleteListener
+// };
+// module.exports.jobDocumentsFirestoreListener = {
+//   jobDocumentCreateListener: jobDocumentCreateListener,
+//   jobDocumentUpdateListener: jobDocumentUpdateListener,
+//   jobDocumentDeleteListener: jobDocumentDeleteListener
+// };
+// module.exports.firmwaresFirestorListener = {
+//   firmwareCreateListener: firmwareCreateListener,
+//   firmwareUpdateListener: firmwareUpdateListener,
+//   firmwareDeleteListener: firmwareDeleteListener
+// };
--- src/pages/Account/Settings/index.js
+++ src/pages/Account/Settings/index.js
@@ -1,65 +1,65 @@
 import React from 'react';
 import {
-	List,
-	ListItem,
-	ListItemIcon,
-	ListItemText,
-	ListItemSecondaryAction
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemSecondaryAction
 } from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
 import { useDispatch } from 'react-redux';
 import {
-	Brightness4 as Brightness4Icon,
-	Home as HomeIcon
+  Brightness4 as Brightness4Icon,
+  Home as HomeIcon
 } from '@material-ui/icons';
 import { updateSettings } from '../../../store/actions/auth-user';
 
 const Settings = (props) => {
-	const authUser = props.authUser;
-	const dispatch = useDispatch();
+  const authUser = props.authUser;
+  const dispatch = useDispatch();
 
-	const switchChangeHandler = async (settings) => {
-		await dispatch(updateSettings(settings));
-	};
+  const switchChangeHandler = async (settings) => {
+    await dispatch(updateSettings(settings));
+  };
 
-	return (
-		<List>
-			<ListItem>
-				<ListItemIcon>
-					<Brightness4Icon />
-				</ListItemIcon>
-				<ListItemText primary='Dark mode' />
-				<ListItemSecondaryAction>
-					<Switch
-						checked={authUser.settings.darkMode}
-						onChange={() =>
-							switchChangeHandler({
-								...authUser.settings,
-								darkMode: !authUser.settings.darkMode
-							})
-						}
-					/>
-				</ListItemSecondaryAction>
-			</ListItem>
-			<ListItem>
-				<ListItemIcon>
-					<HomeIcon />
-				</ListItemIcon>
-				<ListItemText primary='Work from Home' />
-				<ListItemSecondaryAction>
-					<Switch
-						checked={authUser.settings.workFromHome}
-						onChange={() =>
-							switchChangeHandler({
-								...authUser.settings,
-								workFromHome: !authUser.settings.workFromHome
-							})
-						}
-					/>
-				</ListItemSecondaryAction>
-			</ListItem>
-		</List>
-	);
+  return (
+    <List>
+      <ListItem>
+        <ListItemIcon>
+          <Brightness4Icon />
+        </ListItemIcon>
+        <ListItemText primary='Dark Mode' />
+        <ListItemSecondaryAction>
+          <Switch
+            checked={authUser.settings.darkMode}
+            onChange={() =>
+              switchChangeHandler({
+                ...authUser.settings,
+                darkMode: !authUser.settings.darkMode
+              })
+            }
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem>
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary='Work from Home' />
+        <ListItemSecondaryAction>
+          <Switch
+            checked={authUser.settings.workFromHome}
+            onChange={() =>
+              switchChangeHandler({
+                ...authUser.settings,
+                workFromHome: !authUser.settings.workFromHome
+              })
+            }
+          />
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  );
 };
 
 export default Settings;
